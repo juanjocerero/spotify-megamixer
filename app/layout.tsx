ef96067d7adff6577e4c16bc6e05ad5f8921aa10
Juanjo Cerero <elquefaltaba@gmail.com>
2025-07-18 21:01:32 +0200

    removed nextjs font usage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
 import { Toaster } from "@/components/ui/sonner";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Spotify Megamixer",
@@ -26,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="es" className="dark">
     <body
-    className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    className="antialiased"
     >
     {children}
     <Toaster richColors theme="dark" />
