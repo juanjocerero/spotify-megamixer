036703d53a2d23c26f51f196b7aa303a32e5f1ea
Juanjo Cerero <elquefaltaba@gmail.com>
2025-07-23 01:58:38 +0200

    changed default font to Inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
+// app/layout.tsx
+import { Inter } from 'next/font/google';
 import type { Metadata } from "next";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
+import { cn } from '@/lib/utils';
+
+const inter = Inter({ 
+  subsets: ['latin'],
+  variable: '--font-sans',
+  weight: ['100', '300', '400', '500', '700', '900'],
+});
 
 export const metadata: Metadata = {
   title: "Spotify Megamixer",
@@ -13,8 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es" className="dark scrollbar-thin scrollbar-thumb-zinc-700 hover:scrollbar-thumb-zinc-600 scrollbar-track-zinc-900">
-    <body className="antialiased">
+    <html 
+    suppressHydrationWarning
+    lang="es" 
+    className={inter.variable}
+    >
+    <body className={cn(
+      "antialiased font-sans dark scrollbar-thin scrollbar-thumb-zinc-700 hover:scrollbar-thumb-zinc-600 scrollbar-track-zinc-900"
+    )}>
     {children}
     <Toaster richColors theme="dark" />
     </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
-// /tailwind.config.js
-
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
 module.exports = {
   darkMode: ["class"],
   content: [
@@ -18,6 +19,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
