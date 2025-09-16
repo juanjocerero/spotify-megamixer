9d791a8a4eb9fbb083ce428e1326b52d82a9aa43
Juanjo Cerero <elquefaltaba@gmail.com>
2025-09-16 21:50:18 +0000

    typo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,5 @@
 // Carga las variables del fichero .env en el entorno de ESTE MISMO SCRIPT
 require('dotenv').config({ path: '/home/juanjocerero/apps/megamixer/.env' });
-=======
-require('dotenv').config();
->>>>>>> d73ad7d465c0a3dca2ccc88b0ecb859693235d88
 
 module.exports = {
   apps: [
