c8581d136ff6ca1bc7be3f2fdb33722e086b953d
Juanjo Cerero <elquefaltaba@gmail.com>
2025-09-16 21:17:43 +0000

    fixes to ecosystem config and installation of dotenv to read environment variables

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,23 @@
+// Carga las variables del fichero .env en el entorno de ESTE MISMO SCRIPT
+require('dotenv').config({ path: '/home/juanjocerero/apps/megamixer/.env' });
+
 module.exports = {
   apps: [
     {
-      name: 'spotify-megamixer',
+      name: 'megamixer',
       script: 'npm',
       args: 'start',
       interpreter: 'none',
       env: {
         NODE_ENV: 'production',
+	      PORT: 3001,
+        DATABASE_URL: process.env.DATABASE_URL,
+        AUTH_URL: process.env.AUTH_URL,
+        AUTH_SECRET: process.env.AUTH_SECRET,
+        AUTH_SPOTIFY_ID: process.env.AUTH_SPOTIFY_ID,
+        AUTH_SPOTIFY_SECRET: process.env.AUTH_SPOTIFY_SECRET,
+        RESEND_API_KEY: process.env.RESEND_API_KEY,
+        EMAIl_FROM: process.env.EMAIL_FROM
       },
     },
   ],
