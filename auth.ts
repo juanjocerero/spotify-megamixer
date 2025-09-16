2f0e32c8fc40d987ae4f076e46991ad6e8237a36
Juanjo Cerero <elquefaltaba@gmail.com>
2025-09-17 00:06:14 +0200

    pass spotify scopes as array instead of a single string

--- auth.ts
+++ auth.ts
@@ -4,30 +4,34 @@ import { db } from "./lib/db";
 
 const spotifyScopes = 'user-read-email playlist-read-private playlist-modify-public playlist-modify-private user-read-currently-playing';
 
+if (!process.env.AUTH_SPOTIFY_ID || !process.env.AUTH_SPOTIFY_SECRET) {
+  throw new Error("Missing AUTH_SPOTIFY_ID or AUTH_SPOTIFY_SECRET environment variables");
+}
+
 export const auth = betterAuth({
   database: prismaAdapter(db, {
     provider: "postgresql",
   }),
   socialProviders: {
     spotify: {
-      clientId: process.env.AUTH_SPOTIFY_ID!,
-      clientSecret: process.env.AUTH_SPOTIFY_SECRET!,
-      scopes: [spotifyScopes],
+      clientId: process.env.AUTH_SPOTIFY_ID,
+      clientSecret: process.env.AUTH_SPOTIFY_SECRET,
+      scopes: spotifyScopes.split(' '),
     },
   },
   account: {
-      fields: {
-          accountId: "providerAccountId",
-          refreshToken: "refresh_token",
-          accessToken: "access_token",
-          accessTokenExpiresAt: "expires_at",
-          idToken: "id_token",
-      }
+    fields: {
+      accountId: "providerAccountId",
+      refreshToken: "refresh_token",
+      accessToken: "access_token",
+      accessTokenExpiresAt: "expires_at",
+      idToken: "id_token",
+    }
   },
   session: {
-      fields: {
-          expiresAt: "expires",
-          token: "sessionToken"
-      }
+    fields: {
+      expiresAt: "expires",
+      token: "sessionToken"
+    }
   },
 });
